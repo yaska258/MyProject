82ccbdc54d3031c1111e88e5f255da334e1bd0fa
Nisha Shaikh <111333829+nishaadeel@users.noreply.github.com>
2023-04-02 02:57:11 -0500

    make change

--- RG.tf
+++ RG.tf
@@ -2,13 +2,3 @@ resource "azurerm_resource_group" "wordpress" {
   name     = "example-resources"
   location = var.location
 }
-
-
-
-# Generates a random permutation of alphanumeric characters
-resource "random_string" "fqdn" {
-  length  = 6
-  special = false
-  upper   = false
-  number  = false
-}

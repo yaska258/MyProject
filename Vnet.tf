90b149abbfa450e21e773510b49633ac8516c1fb
Nisha Shaikh <111333829+nishaadeel@users.noreply.github.com>
2023-04-02 02:49:16 -0500

    update Vnet

--- Vnet.tf
+++ Vnet.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "wordpress" {
-  name     = "example-resources"
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "wordpress" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]

aef836f58a0ea7fceb81dfa3ea3ca35e4751778f
yaska258 <121067455+yaska258@users.noreply.github.com>
2023-04-03 16:15:34 -0500

    removed backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-eastus"
-    storage_account_name = "cs210032002341f2ce4"
-    container_name       = "tfstate"
-    key                  = "MYPROJECT.terraform.tfstate"
-  }
-}

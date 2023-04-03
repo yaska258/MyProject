876eec96abad5dd14b0f0bd7c4363f0ab974b3dd
Nisha Shaikh <111333829+nishaadeel@users.noreply.github.com>
2023-04-03 14:24:08 -0500

    chnaged ssl to true

--- Mysql.tf
+++ Mysql.tf
@@ -15,7 +15,7 @@ resource "azurerm_mysql_server" "wordpress" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
+  ssl_enforcement_enabled           = true
   #ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
